c30df38711cb10ab77a37da924808dd80e9fb74b
SBouchier <simonbouchier1@gmail.com>
2018-08-17 16:27:58 +0100

    First working version

--- SQL2017/SQL2017.ps1
+++ SQL2017/SQL2017.ps1
@@ -5,28 +5,39 @@ Function Install-SQL2017 {
         [ValidateScript({Test-Path $_})]
         [string]$SQLFilePath,
 
+        #todo: if path does not exist, create it
         [Parameter(Mandatory=$True)]
         [ValidateScript({Test-Path $_})]
         [string]$installPath,
-        
+
         [ValidateSet('SQLauth', 'WindowsAuth', 'Both')]
         [Parameter(Mandatory=$True)]
         [string]$authenticationType,
 
+        #todo: use this param
         [Parameter(Mandatory=$True)]
         [ValidateSet('Basic', 'Custom', 'Download Media')]
         [string]$SQLversion,
 
+        #todo: use this param
         [Parameter(Mandatory=$True)]
-        [ValidateSet('feature1', 'feature2', 'feature3')]
-        [string]$SQLfeatures
+        [ValidateSet('SQL', 'AS', 'RS', 'RS_SHP', 'RS_SHPWFE', 'DQC', 'IS', 'MDS', 'SQL_SHARED_MPY', 'SQL_SHARED_MR', 'Tools')]
+        [string[]]$SQLfeatures
     )
 
-    & 'C:\Users\Simon\Downloads\ghostwolf\SQLServer2017-SSEI-Expr.exe'
+    $featureList
+    foreach ($i in $SQLfeatures){
+        $featureList = $featureList + $i + ","
+        Write-Host $i
+    }
+    $featureList = $featureList.Substring(0,$featureList.Length-1)
+    Write-Host $featureList
+
+    & $SQLFilePath /q /v /ACTION=Install /INSTALLPATH=$installPath /IACCEPTSQLSERVERLICENSETERMS /LANGUAGE=en-US #/HELP=True
 }
 
 #link to Documentation listing SQL cmdline install params:
 #https://docs.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-2017
 
-& 'C:\Users\Simon\Downloads\ghostwolf\SQLServer2017-SSEI-Expr.exe' /ACTION=Install /FEATURES=SQL /INSTANCENAME="Test" /AGTSVCACCOUNT "" /AGTSVCPASSWORD "" /ASSVCACCOUNT "" /ASSVCPASSWORD "" /ASSYSADMINACCOUNTS "" /SQLSVCACCOUNT "" /SQLSVCPASSWORD "" /SQLSYSADMINACCOUNTS "" /ISSVCACCOUNT "" /ISSVCPASSWORD "" /RSSVCACCOUNT "" /RSSVCPASSWORD ""
- 
+Install-SQL2017 C:\Users\Simon\Downloads\SQLTest\SQLServer2017-SSEI-Expr.exe C:\Users\Simon\Downloads\SQLTest 'SQLauth' 'Basic' 'SQL','AS','DQC'
+
