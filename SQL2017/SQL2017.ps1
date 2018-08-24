bafe93a02af6d253d1613b3d86d4c6a743c4d889
SBouchier <simonbouchier1@gmail.com>
2018-08-24 14:43:07 +0100

    Code is now runnable, added working SQLauth as an option

--- SQL2017/SQL2017.ps1
+++ SQL2017/SQL2017.ps1
@@ -9,44 +9,59 @@ Function Install-SQL2017 {
         [ValidateScript({Test-Path $_})]
         [string]$installPath,
 
-        #todo: use this param
-        [ValidateSet('WindowsAuth', 'Mixed')]
-        [Parameter(Mandatory=$True)]
-        [string]$authenticationType,
-
         #todo: use this + validate if it's a valid SQL version
         [Parameter(Mandatory=$True)]
         [string]$SQLversion,
 
         #todo: use this param
         [Parameter(Mandatory=$True)]
-        [ValidateSet('SQL', 'AS', 'RS', 'RS_SHP', 'RS_SHPWFE', 'DQC', 'IS', 'MDS', 'SQL_SHARED_MPY', 'SQL_SHARED_MR', 'Tools', 'SQLENGINE')]
-        [string[]]$SQLfeatures
+        [ValidateSet('SQL', 'AS', 'RS', 'RS_SHP', 'RS_SHPWFE', 'DQC', 'IS', 'MDS', 'SQL_SHARED_MPY', 'SQL_SHARED_MR', 'Tools', 'SQLENGINE', 'REPLICATION', 'FULLTEXT', 'BC','SDK')]
+        [string[]]$features,
+
+        #if switch is called, user can give a password for the SQL auth
+        [Parameter(ParameterSetName='Auth', Mandatory=$false)]
+        [switch]$SQLauthentication,
+        #[ValidateSet('WindowsAuth', 'Mixed', 'SQL')]
+        [Parameter(ParameterSetName='Auth', Mandatory=$false)]
+        [string]$SQLpwd
     )
+
 
     #code to format the input of features into the correct style for input (CSV/no spaces)
     $featureList
-    foreach ($i in $SQLfeatures){
+    foreach ($i in $features){
         $featureList = $featureList + $i + ","
-        Write-Host $i
     }
     $featureList = $featureList.Substring(0,$featureList.Length-1)
     Write-Host $featureList
 
 
-    #cd "C:\Program Files\Microsoft SQL Server\140\Setup Bootstrap\SQL2017\x64"
-    #.\LandingPage.exe /HELP=true
     
+    #code that handles the option for user to have either SQL auth as well as windows Auth, or just windows Auth.
+    If ($SQLauthentication.IsPresent){
+        Set-OrAddIniValue -FilePath ".\ConfigurationFile.ini"  -keyValueList @{
+            SECURITYMODE='"SQL"'
+            SAPWD='"'+$SQLpwd+'"'
+        }
+    }
+    Else{
+        Write-Host 'nope'
+        Set-OrAddIniValue -FilePath ".\ConfigurationFile.ini"  -keyValueList @{
+            SECURITYMODE=""
+            SAPWD=""
+        }
+    }
+
 
     #code that is able to modify the ConfigFile.ini to contain whatever inputs you wish before it's used below.
     Set-OrAddIniValue -FilePath ".\ConfigurationFile.ini"  -keyValueList @{
-        INSTANCEID = '"SQLEXPRESS_AAA"'
+        INSTANCEID = '"SQLEXPRESS_ABC"'
         FEATURES=$featureList
         INSTALLSHAREDDIR='"C:\Users\Simon\Downloads\SQLtest"'
     }
   
     #2.0: uses the ConfigFile in the same directory as this .ps1 file
-    & $SQLFilePath /v /ACTION=Install /INSTALLPATH=$installPath /LANGUAGE=en-US /CONFIGURATIONFILE="ConfigurationFile.ini" /IACCEPTROPENLICENSETERMS /IACCEPTSQLSERVERLICENSETERMS #/HELP=True
+    #& $SQLFilePath /v /ACTION=Install /INSTALLPATH=$installPath /LANGUAGE=en-US /CONFIGURATIONFILE="ConfigurationFile.ini" /IACCEPTROPENLICENSETERMS /IACCEPTSQLSERVERLICENSETERMS #/HELP=True
     
     #1.0: a version that does not use ConfigFile.ini and attempts to pass everything via params
     #& $SQLFilePath /ACTION=Install /INSTALLPATH=$installPath /FEATURES="SQLENGINE,REPLICATION,SQL_INST_MR,FULLTEXT,CONN,BC,SDK" /INSTANCENAME=SQLEXPRESSAAA /AGTSVCACCOUNT="NT AUTHORITY\NETWORK SERVICE" /AGTSVCSTARTUPTYPE="Disabled" /SQLSVCACCOUNT="NT Service\MSSQL$SQLEXPRESS" /SQLSYSADMINACCOUNTS="BUILTIN\ADMINISTRATORS" /SECURITYMODE="SQL" /SAPWD="mypasswordhere" /IACCEPTSQLSERVERLICENSETERMS /IACCEPTROPENLICENSETERMS
@@ -77,8 +92,8 @@ function Set-OrAddIniValue {
 
 
 
-Install-SQL2017 C:\Users\Simon\Downloads\SQLTest\SQLServer2017-SSEI-Expr.exe D:\SQL 'Mixed' 'Basic' 'SQL','REPLICATION','FULLTEXT','BC','SDK'
-
+Install-SQL2017 -SQLFilePath C:\Users\Simon\Downloads\SQLTest\SQLServer2017-SSEI-Expr.exe -installPath D:\SQL -SQLversion 'Basic' -features 'SQL','FULLTEXT','BC','SDK' -SQLauthentication -SQLpwd 'test'
+#Install-SQL2017 -SQLFilePath C:\Users\Simon\Downloads\SQLTest\SQLServer2017-SSEI-Expr.exe -installPath D:\SQL -SQLversion 'Basic' -features 'SQL','FULLTEXT','BC','SDK'
 
 #link to Documentation listing SQL cmdline install params:
 #https://docs.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-2017
